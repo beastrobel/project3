b37f64c553bab5403e0da873e3c301e8cd29f9d5
beastrobel <beatrizstrobel@proton.me>
2024-01-20 20:26:05 -0500

    Connected client and server sides

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Homepage from './pages/homepage/Homepage';
+ 
+function App() {
+    return (
+        <>
+        <Navbar/>
+        <Homepage />
+        <Footer/>
+        </>
+    );    
+}
+
+export default App;
