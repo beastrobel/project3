db838ecb256014f7d068fbb93f76941fd20eee65
beastrobel <beatrizstrobel@proton.me>
2024-01-30 16:13:05 -0500

    Debugging for deployment

--- server/server.js
+++ server/server.js
@@ -50,10 +50,10 @@ const startApolloServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../client')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/index.html'));
     });
   }
 
