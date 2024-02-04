a74754df3f93a1cf909db7fcf6986cd0df61de34
beastrobel <beatrizstrobel@proton.me>
2024-02-04 01:37:00 -0500

    Optimized app, removed unused dependencies

--- client/src/components/Theme.jsx
+++ client/src/components/Theme.jsx
@@ -2,8 +2,8 @@ import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
   fonts: {
-    heading: `'Figtree Variable', sans-serif`,
-    body: `'Figtree Variable', sans-serif`,
+    heading:  'Figtree',
+    body:  'Figtree',
   },
 })
 
